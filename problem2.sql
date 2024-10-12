e486fe1a08641006b968c6595fbd7de0f3f97b38
Angela Flynn <Angela.Flynn@keyin.com>
2024-10-12 05:34:47 -0230

    Inserted values into tables for problem 2

--- problem2.sql
+++ problem2.sql
@@ -29,3 +29,31 @@ CREATE TABLE order_items (
     quantity INT,
     PRIMARY KEY (order_id, product_id)
 );
+
+
+-- Insert Data
+
+INSERT INTO products (product_name, price, stock_quantity)
+VALUES
+	('Dog bed', 49.99, 10),
+	('Kong', 19.99, 15),
+	('Collar',12.99, 20),
+	('Leash', 15.99, 20),
+	('Crate', 59.99, 10);
+
+INSERT INTO customers (first_name, last_name, email)
+VALUES
+	('Mary','Sue', 'msue@example.com'),
+	('Billy','Bob', 'billy@example.com'),
+	('Cesar', 'Millan','dogwhisperer@example.com'),
+	('Oprah','Winfrey','yougetacar@example.com');
+
+INSERT INTO orders (customer_id, order_date)
+VALUES
+	(1,'2024-08-16'),
+	(2,'2024-09-20'),	
+	(3,'2024-09-21'),
+	(1,'2024-10-01'),
+	(4,'2024-10-10');
+
+
