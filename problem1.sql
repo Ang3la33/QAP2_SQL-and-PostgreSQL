9756d1dca057aec5673d5112021c93ad9c575c76
Angela Flynn <Angela.Flynn@keyin.com>
2024-10-12 03:29:12 -0230

    Inserted Data into tables

--- problem1.sql
+++ problem1.sql
@@ -28,3 +28,25 @@ CREATE TABLE enrollments (
 	enrollment_date DATE,
 	PRIMARY KEY (student_id, course_id)
 );
+
+INSERT INTO students (first_name, last_name, email, school_enrollment_date)
+VALUES
+	('Jane', 'Doe', 'janedoe@example.com', '2024-01-01'),
+	('John', 'Doe', 'johnny@example.com', '2024-01-01'),
+	('Diane', 'Ross', 'dedeross@example.com', '2024-04-20'),
+	('Bob', 'Ross', 'happylittleaccidents@example.com', '2024-04-20'),
+	('Marshall','Mathers', 'realslimshady@example.com', '2000-04-18');
+	
+INSERT INTO professors (first_name, last_name, department)
+VALUES
+	('Charles', 'Xavier','Philosophy'),
+	('Stephen','Hawking','Science'),
+	('Virginia','Woolf','English'),
+	('Gerda','Lerner','History');
+
+INSERT INTO courses (course_name, course_description, professor_id)
+VALUES
+	('Ethics 101', 'Intro to moral philosophy and the ethical use of power', 1),
+	('Physics 101', 'Black holes and the big bang theory', 2),
+	('Literature 101', 'Modernist and feminist literature', 3);
+
