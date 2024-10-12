924e4e626f21b565dad200d423701aa793730dd6
Angela Flynn <Angela.Flynn@keyin.com>
2024-10-12 03:52:28 -0230

    Write SQL Queries

--- problem1.sql
+++ problem1.sql
@@ -1,5 +1,8 @@
 -- Problem 1 - University Course Enrollment System
 
+
+-- Create Tables
+
 CREATE TABLE students (
     id SERIAL PRIMARY KEY,
     first_name TEXT,
@@ -29,6 +32,9 @@ CREATE TABLE enrollments (
 	PRIMARY KEY (student_id, course_id)
 );
 
+
+-- Insert Data
+
 INSERT INTO students (first_name, last_name, email, school_enrollment_date)
 VALUES
 	('Jane', 'Doe', 'janedoe@example.com', '2024-01-01'),
@@ -49,4 +55,35 @@ VALUES
 	('Ethics 101', 'Intro to moral philosophy and the ethical use of power', 1),
 	('Physics 101', 'Black holes and the big bang theory', 2),
 	('Literature 101', 'Modernist and feminist literature', 3);
+
+INSERT INTO enrollments (student_id, course_id, enrollment_date)
+VALUES
+	((SELECT id FROM students WHERE first_name = 'Jane' AND last_name = 'Doe'), 1, '2024-09-01'),
+	(1,2,'2024-09-01'),
+	(1,3,'2024-01-01'),
+	(5,1,'2024-09-20'),
+	(2,2,'2024-09-20'),
+	(2,3,'2024-01-01'),
+	(4,2,'2024-01-01'),
+	(3,1,'2024-01-02'),
+	(3,2,'2024-01-02');
+
+
+-- Write SQL Queries
+
+-- Retrieve full names of all students enrolled in "Physics 101"
+SELECT first_name || ' ' || last_name AS full_name
+FROM students
+JOIN enrollments ON students.id = enrollments.student_id
+JOIN courses ON enrollments.course_id = courses.id
+WHERE courses.course_name = 'Physics 101';
+
+-- Retrieve a list of all courses along with the professor's full name who teachers each course
+SELECT courses.course_name, first_name || ' ' || last_name AS full_name
+FROM courses
+JOIN professors ON courses.professor_id = professors.id;
+
+
+
+
 
