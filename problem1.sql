f4bb89dd99e2d8b36b62ee5e3368bec390a5acc5
Angela Flynn <Angela.Flynn@keyin.com>
2024-10-12 04:05:01 -0230

    Delete data

--- problem1.sql
+++ problem1.sql
@@ -99,6 +99,7 @@ WHERE first_name = 'Bob' AND last_name = 'Ross';
 -- Delete Data
 
 -- Remove a student from one of their courses
-
+DELETE FROM enrollments
+WHERE student_id = 1 AND course_id = 1;
 
 
